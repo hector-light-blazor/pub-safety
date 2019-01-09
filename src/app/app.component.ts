c0e3769aacd588ecb63b8a7e3632f9226cbc05b8
hchapa <hchapa@lrgvdc911.org>
2019-01-09 16:21:50 -0600

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+//import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+const { SplashScreen } = Plugins;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -24,7 +25,7 @@ export class AppComponent {
 
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
+    //private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
@@ -33,7 +34,8 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      //this.splashScreen.hide();
+      SplashScreen.hide();
     });
   }
 }
